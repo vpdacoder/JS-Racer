bc8b247da6099ac718f8a560d026a606b839d2c8
vpdacoder <vaipatel@utexas.edu>
2018-03-25 17:41:53 -0500

    Got Player One To Move

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-alert("I'm working!");
+// alert("I'm working!");
+
+let p1 = document.getElementById('player1');
+let p2 = document.getElementById('player2');
+
+
+//make array to push values from car movements
+boxOne = [];
+boxTwo = [];
+
+// const movePlayerOne = () => {
+//   document.getElementsByClassName('red').classList.remove('red').nextElementSibling.classList.add('red');
+// }
+
+// document.addEventListener('click', () => {
+//   console.log("PRESSED");
+//   let first = document.querySelector("red");
+//   first.classList.remove("red");
+// })
+
+
+let current = document.querySelector("td");
+
+
+document.addEventListener('click', () => {
+  console.log("PRESSED");
+  current.classList.remove("red");
+  current.nextElementSibling.classList.add("red");
+  current = current.nextElementSibling;
+  boxOne.push(1);
+  if (boxOne.length > 18) {
+    alert("Player 1 Won");
+  }
+
+
+// document.getElementsByClassName('className');
+});
+
+
+
+
+
+// document.keydown = moveBox;
+
+
+
+// button.addEventListener("click", () => {
+// document.body.classList.toggle("back");
+// });
